c4b1809e4944b190f89e5255488e1a35d1189a83
Arif Mohammed <arifmohammed.technical@gmail.com>
2025-08-30 02:09:38 -0500

    enabled assets folder; new svg log for banner; updated header component with background image, logo and text

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
-  standalone: true
+  styleUrls: ['./header.component.scss', '../../app.scss'],
+  standalone: true,
+    imports: [
+        NgClass,
+        RouterLink,
+    ]
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    dirty: boolean;
+    envName: string;
+    appVersion: string;
+
+    constructor() {
+        this.dirty = false;
+        this.envName = 'Local';
+        this.appVersion = '0.0.1-SNAPSHOT';
+    }
+}
