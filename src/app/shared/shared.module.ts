00b64e40e83d4882c48a1f53db5ec199f56b2de2
Arif Mohammed <arifmohammed.technical@gmail.com>
2025-08-30 00:59:16 -0500

    added footer component and included it in app.html

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HeaderComponent],
-  exports: [HeaderComponent]
+  imports: [CommonModule, HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent]
 })
 export class SharedModule {}
